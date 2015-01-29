7f2ee940f8f7b4f6ef436d033e9ac3f660a1c498
James Pinkster <james@jp-it.net.au>
2015-01-29 18:02:25 +1100

    temporarily removed runaquery

--- runaquery/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class QueryOptions(models.Model):
-    biginteger = models.BigIntegerField()
-
-    def __unicode__(self):
-        return str(self.biginteger)
-
--- runaquery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from runaquery import views
-
-# "url" "name" argument allows reference from templates
-urlpatterns = patterns('',
-    url(r'^enterdata/$', views.enterdata, name='enterdata'),
-)
