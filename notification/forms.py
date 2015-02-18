2b1210b475d174fb4802803b98dae5e9cc07b0c7
James Pinkster <jamesp9@users.noreply.github.com>
2015-02-18 15:30:37 +1100

    Update forms.py

--- notification/forms.py
+++ notification/forms.py
@@ -18,7 +18,7 @@ class NotificationForm(forms.ModelForm):
 
     class Meta:
         model = Notification
-        fields = ['headline']
+        fields = ['client', 'ntype', 'headline']
 
 
 class StartNotificationForm(forms.Form):
