7649425a78ca3b755f714ad081edd4511b19ed4d
James Pinkster <james@jp-it.net.au>
2015-02-20 23:02:18 +1100

    Fixed bug, when no updates existed

--- notification/forms.py
+++ notification/forms.py
@@ -2,17 +2,6 @@ from django import forms
 
 from .models import Notification, Update
 
-SUITE1_CLIENT_CHOICES = (
-        ('client01', 'Client 01'), 
-        ('client02', 'Client 02'), 
-        ('client03', 'Client 03'),
-        )
-
-NOTIFICATION_CHOICES = (
-        ('sev1', 'Severity 1'),
-        ('hot_issue', 'Hot Issue'),
-        )
-
 
 class NotificationForm(forms.ModelForm):
 
@@ -27,18 +16,7 @@ class UpdateForm(forms.ModelForm):
         model = Update
 
 
-class StartNotificationForm(forms.Form):
-    client_name = forms.ChoiceField(label='Client', choices=SUITE1_CLIENT_CHOICES)
-    noti_type = forms.ChoiceField(label='Notification', choices=NOTIFICATION_CHOICES)
-
-class NotificationInformationForm(forms.Form):
-    ticket_no = forms.CharField(max_length=100)
-    headline = forms.CharField(max_length=255)
-    requestor_name = forms.CharField(max_length=100)
-
-# class NotiUpdateForm(forms.Form):
-#     update_number = forms.IntegerField()
-#     update_time = forms.DateTimeField()
-#     update_person = forms.CharField(max_length=255)
-#     update_text = forms.CharField(widget=forms.Textarea)
-#     next_update_time = forms.DateTimeField()
+# class NotificationInformationForm(forms.Form):
+#     ticket_no = forms.CharField(max_length=100)
+#     headline = forms.CharField(max_length=255)
+#     requestor_name = forms.CharField(max_length=100)
--- notification/views.py
+++ notification/views.py
@@ -9,25 +9,6 @@ from .models import Notification, Update
 from .forms import NotificationForm, UpdateForm
 
 
-#def start_notification(request):
-#    # if this is a POST request we need to process the form data
-#    if request.method == 'POST':
-#        # create a form instance and populate it with data from the request:
-#        form = StartNotificationForm(request.POST)
-#        print(request.POST)
-#        # check whether it's valid:
-#        if form.is_valid():
-#            # process the data in form.cleaned_data as required
-#            # ...
-#            # redirect to a new URL:
-#            return HttpResponseRedirect('/notification/notification_information/')
-#
-#    # if a GET (or any other method) we'll create a blank form
-#    else:
-#        form = StartNotificationForm()
-#
-#    return render(request, 'notification/start_notification.html', {'form': form})
-
 def notifications(request):
     return render_to_response(
         'notification/notifications.html', {'notifications': Notification.objects.all()})
@@ -114,16 +95,22 @@ def add_update(request, notification_id):
             return HttpResponseRedirect('/notification/' + notification_id + '/')
 
     else:
-        next_update_at = timezone.now() + timedelta(hours=1)
         number_of_updates = len(Update.objects.filter(notification__id=notification_id))
+
         if number_of_updates > 0:
-            next_update_is_due = Update.objects.filter(notification__id=notification_id).get(update_number=number_of_updates).next_update_at
-            next_update_at = next_update_is_due + timedelta(hours=1)
+            # Prefill "updated at" with the "next update" time from previous Update
+            updated_at = Update.objects.filter(notification__id=notification_id).get(update_number=number_of_updates).next_update_at
+        else:
+            updated_at = timezone.now()
+
+        next_update_at = updated_at + timedelta(hours=1)
+
         update_number = number_of_updates + 1
+
         form = UpdateForm(initial={
             'notification': notification_id,
             'update_number': update_number,
-            'updated_at': next_update_is_due,
+            'updated_at': updated_at,
             'next_update_at': next_update_at, })
 
         # render used instead of render_to_response as that resulted in 403 error
@@ -131,42 +118,6 @@ def add_update(request, notification_id):
         return render(request, 'notification/add_update.html', {'form': form, 'notification': notification_id, })
 
 
-
 def preview_email(request):
     client = request.GET.get('client', '')
     return render(request, 'notification/preview_email.html', {'client': client})
-
-
-
-# From django site second Work With Forms example
-#def contact(request):
-#    # if this is a POST request we need to process the form data
-#    if request.method == 'POST':
-
-# From django site second Work With Forms example
-#def contact(request):
-#    # if this is a POST request we need to process the form data
-#    if request.method == 'POST':
-#        # create a form instance and populate it with data from the request:
-#        form = ContactForm(request.POST)
-#        # check whether it's valid:
-#        if form.is_valid():
-#            subject = form.cleaned_data['subject']
-#            message = form.cleaned_data['message']
-#            sender = form.cleaned_data['sender']
-#            cc_myself = form.cleaned_data['cc_myself']
-#
-#            recipients = ['info@example.com']
-#            if cc_myself:
-#                recipients.append(sender)
-#
-#            send_mail(subject, message, sender, recipients)
-#            return HttpResponseRedirect('/workingwithforms/thanks/')
-#    # if a GET (or any other method) we'll create a blank form
-#    else:
-#        form = ContactForm()
-#
-#    #return render(request, 'workingwithforms/contact_manual_fields.html', {'form': form})
-#    return render(request, 'workingwithforms/contact.html', {'form': form})
-
-
