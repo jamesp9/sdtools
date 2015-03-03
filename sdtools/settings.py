9e709c28af6197f102867708b2dc06d839a5fce1
James Pinkster <james@jp-it.net.au>
2015-03-04 10:41:30 +1100

    Added base.css and got html to extent base.html

--- sdtools/settings.py
+++ sdtools/settings.py
@@ -87,10 +87,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 TEMPLATE_LOADERS = (
