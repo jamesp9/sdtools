ee80e96b9ce209bfb836174aafe421b28bb1a7ab
jamesp9 <james@exitstatus0.net>
2021-09-26 22:37:54 +1000

    django 3.2.7

--- sdtools/sdtools/settings.py
+++ sdtools/sdtools/settings.py
@@ -10,20 +10,34 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
+
+# django-environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(f'BASE_DIR: {BASE_DIR}')
 
+# Take environment variables from .env file
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env_path = str(Path(BASE_DIR, '.env'))
+print(f'env_path: {env_path}')
+environ.Env.read_env(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--w7iev00b$48(6aro$ajkt)t71nfv!t12oa7*3a*3%m#nb7eel'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +68,7 @@ ROOT_URLCONF = 'sdtools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
